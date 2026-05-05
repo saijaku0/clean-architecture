b9b160f93424e042f418730fbdfbef3a04438950
Vadym <minerking112256@gmail.com>
2026-05-05 18:52:21 +0200

    feat: add domain events interceptor

--- src/TrainBooking.Infrastructure/InfrastructureServiceExtensions.cs
+++ src/TrainBooking.Infrastructure/InfrastructureServiceExtensions.cs
@@ -3,6 +3,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using TrainBooking.Application.Abstractions.Repositories;
 using TrainBooking.Infrastructure.Persistence;
+using TrainBooking.Infrastructure.Persistence.Interceptors;
 using TrainBooking.Infrastructure.Persistence.Repositories;
 
 namespace TrainBooking.Infrastructure;
@@ -15,9 +16,13 @@ public static class InfrastructureServiceExtensions
     {
         string connectionString = configuration.GetConnectionString("DefaultConnection")
             ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-        services.AddDbContext<AppDbContext>(options =>
-            options.UseSqlServer(connectionString, b =>
-                b.MigrationsAssembly("TrainBooking.Migrations")));
+        services.AddDbContext<AppDbContext>((sp, options) =>
+        {
+            options.UseSqlServer(connectionString, b => b.MigrationsAssembly("TrainBooking.Migrations"));
+            options.AddInterceptors(sp.GetRequiredService<DispatchDomainEventsInterceptor>());
+        });
+
+        services.AddScoped<DispatchDomainEventsInterceptor>();
 
         services.AddScoped<IUnitOfWork, UnitOfWork>();
         services.AddScoped<IUserRepository, UserRepository>();
