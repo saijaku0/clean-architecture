afc37b39fa8f4a63c0b96d009c0e46daf63fe1b4
Vadym <minerking112256@gmail.com>
2026-05-09 12:30:20 +0200

    Update main entry point register pipline

--- src/TrainBooking.Api/Program.cs
+++ src/TrainBooking.Api/Program.cs
@@ -1,5 +1,8 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using TrainBooking.Api.Middleware;
+using TrainBooking.Api.Service;
 using TrainBooking.Application;
+using TrainBooking.Application.Abstractions.Identity;
 using TrainBooking.Infrastructure;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
@@ -15,6 +18,15 @@ builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
 builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddApplication();
 
+builder.Services
+    .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer();
+
+builder.Services.ConfigureOptions<Auth0JwtBearerOptionsConfigurator>();
+
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddScoped<ICurrentUserService, CurrentUserService>();
+
 WebApplication app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -25,8 +37,9 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseExceptionHandler();
+app.UseAuthentication();
 app.UseAuthorization();
-
+app.UseMiddleware<Auth0UserProvisioningMiddleware>();
 app.MapControllers();
 
 app.Run();
