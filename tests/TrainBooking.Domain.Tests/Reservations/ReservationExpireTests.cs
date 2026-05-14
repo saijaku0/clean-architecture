7d0d3c4a374b49ae6b09adb1a2d068d610259c0c
Vadym <minerking112256@gmail.com>
2026-05-14 19:28:54 +0200

    Add two other (empty) tests

---
+++ tests/TrainBooking.Domain.Tests/Reservations/ReservationExpireTests.cs
@@ -0,0 +1,7 @@
+namespace TrainBooking.Domain.Tests.Reservations;
+
+// TO DO: Implement tests for Reservation.Expire method,
+// covering scenarios such as successful expiration and expiration of already expired reservation.
+internal class ReservationExpireTests
+{
+}
