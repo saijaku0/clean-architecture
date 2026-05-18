638805c827eda833d1f38e9e57f953d14ece1be3
Vadym <minerking112256@gmail.com>
2026-05-18 18:00:20 +0200

    Create ConfirmReservationCommand.cs

---
+++ src/TrainBooking.Application/Reservations/Commands/ConfirmReservation/ConfirmReservationCommand.cs
@@ -0,0 +1,12 @@
+using TrainBooking.Application.Abstractions.Commands;
+
+namespace TrainBooking.Application.Reservations.Commands.ConfirmReservation;
+
+public sealed record ConfirmReservationCommand(
+    Guid ReservationId) : ICommand<ConfirmReservationResult>;
+
+public sealed record ConfirmReservationResult(
+    Guid ReservationId,
+    Guid TripId,
+    IReadOnlyCollection<Guid> TripSeatIds,
+    decimal TotalPrice);
